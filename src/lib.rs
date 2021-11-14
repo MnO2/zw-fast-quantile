122e5f446e3e7ed73a78d5fec252da1e8858caf1
Paul Meng <me@paulme.ng>
2021-11-14 09:09:06 -0500

    Remove syntax warnings

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ where
     T: Clone,
 {
     fn new(val: T, rmin: i64, rmax: i64) -> Self {
-        return RankInfo { val, rmin, rmax };
+        RankInfo { val, rmin, rmax }
     }
 }
 
@@ -46,9 +46,9 @@ where
 {
     epsilon: f64,
     b: usize,
-    L: usize,
+    level: usize,
     cnt: usize,
-    S: Vec<Vec<RankInfo<T>>>,
+    s: Vec<Vec<RankInfo<T>>>,
 }
 
 impl<T> EpsilonSummary<T>
@@ -57,70 +57,70 @@ where
 {
     pub fn new(level: usize, epsilon: f64) -> Self {
         let b = ((level as f64) / epsilon + epsilon.ln() / (epsilon * 2.0_f64.ln())).floor() as usize;
-        let S = vec![vec![]; level];
+        let s = vec![vec![]; level];
 
         EpsilonSummary {
-            epsilon: epsilon,
-            b: b,
-            L: level,
+            epsilon,
+            b,
+            level,
             cnt: 0,
-            S: S,
+            s,
         }
     }
 
     pub fn update(&mut self, e: T) {
-        let idx = self.S[0].upper_bound(&RankInfo::new(e.clone(), 0, 0));
+        let idx = self.s[0].upper_bound(&RankInfo::new(e.clone(), 0, 0));
         let rank_info = RankInfo::new(e, idx as i64, idx as i64);
-        self.S[0].insert(idx, rank_info);
+        self.s[0].insert(idx, rank_info);
 
         self.cnt += 1;
-        let n = self.S[0].len();
+        let n = self.s[0].len();
         if n < self.b {
             return;
         }
 
-        for (i, r) in self.S[0].iter_mut().enumerate() {
+        for (i, r) in self.s[0].iter_mut().enumerate() {
             r.rmin = i as i64;
             r.rmax = i as i64;
         }
 
         let compressed_size = self.b / 2;
-        let mut Sc = compress(&self.S[0], compressed_size, self.epsilon);
+        let mut s_c = compress(&self.s[0], compressed_size, self.epsilon);
 
-        self.S[0].clear();
-        let L = self.L;
-        for k in 1..L {
-            if self.S[k].is_empty() {
-                self.S[k] = Sc.clone();
+        self.s[0].clear();
+        let l = self.level;
+        for k in 1..l {
+            if self.s[k].is_empty() {
+                self.s[k] = s_c.clone();
             } else {
-                let t = merge(&self.S[k], &Sc);
-                Sc = compress(&t, compressed_size, self.epsilon);
-                self.S[k].clear();
+                let t = merge(&self.s[k], &s_c);
+                s_c = compress(&t, compressed_size, self.epsilon);
+                self.s[k].clear();
             }
         }
     }
 
     pub fn query(&mut self, e: T) -> f64 {
-        for (i, r) in self.S[0].iter_mut().enumerate() {
+        for (i, r) in self.s[0].iter_mut().enumerate() {
             r.rmin = i as i64;
             r.rmax = i as i64;
         }
 
-        let mut Sm = self.S[0].clone();
-        for i in 1..self.L {
-            Sm = merge(&Sm, &self.S[i])
+        let mut s_m = self.s[0].clone();
+        for i in 1..self.level {
+            s_m = merge(&s_m, &self.s[i])
         }
 
         let mut i = 0;
-        while i < Sm.len() {
-            if Sm[i].val >= e {
+        while i < s_m.len() {
+            if s_m[i].val >= e {
                 break;
             }
 
             i += 1;
         }
 
-        let quantile: f64 = ((Sm[i].rmin + Sm[i].rmax) as f64) / (2.0_f64 * self.cnt as f64);
+        let quantile: f64 = ((s_m[i].rmin + s_m[i].rmax) as f64) / (2.0_f64 * self.cnt as f64);
         if quantile < 0.0_f64 {
             0.0_f64
         } else {
@@ -134,86 +134,102 @@ where
     }
 }
 
-fn merge<T: Clone + Ord>(Sa: &Vec<RankInfo<T>>, Sb: &Vec<RankInfo<T>>) -> Vec<RankInfo<T>> {
-    if Sa.len() == 0 {
-        return Sb.clone();
+fn merge<T: Clone + Ord>(s_a: &[RankInfo<T>], s_b: &[RankInfo<T>]) -> Vec<RankInfo<T>> {
+    if s_a.is_empty() {
+        return s_b.to_vec();
     }
 
-    if Sb.len() == 0 {
-        return Sa.clone();
+    if s_b.is_empty() {
+        return s_a.to_vec();
     }
 
-    let mut Sm = Vec::new();
+    let mut s_m = Vec::new();
 
     let mut i1 = 0;
     let mut i2 = 0;
-    let mut from = 0;
+    let mut from;
 
-    while i1 < Sa.len() || i2 < Sb.len() {
-        let mut val;
-        let mut rmin;
-        let mut rmax;
+    while i1 < s_a.len() || i2 < s_b.len() {
+        let val;
+        let rmin;
+        let rmax;
 
-        if i1 < Sa.len() && i2 < Sb.len() {
-            if Sa[i1].val < Sb[i2].val {
-                val = Sa[i1].val.clone();
+        if i1 < s_a.len() && i2 < s_b.len() {
+            if s_a[i1].val < s_b[i2].val {
+                val = s_a[i1].val.clone();
                 from = 1;
             } else {
-                val = Sb[i2].val.clone();
+                val = s_b[i2].val.clone();
                 from = 2;
             }
-        } else if i1 < Sa.len() && i2 >= Sb.len() {
-            val = Sa[i1].val.clone();
+        } else if i1 < s_a.len() && i2 >= s_b.len() {
+            val = s_a[i1].val.clone();
             from = 1;
         } else {
-            val = Sb[i2].val.clone();
+            val = s_b[i2].val.clone();
             from = 2;
         }
 
         if from == 1 {
-            if 0 < i2 && i2 < Sb.len() {
-                rmin = Sa[i1].rmin + Sb[i2 - 1].rmin;
-                rmax = if Sa[i1].rmax + Sb[i2].rmax > 0 { Sa[i1].rmax + Sb[i2].rmax - 1  } else { 0 }
+            if 0 < i2 && i2 < s_b.len() {
+                rmin = s_a[i1].rmin + s_b[i2 - 1].rmin;
+                rmax = if s_a[i1].rmax + s_b[i2].rmax > 0 {
+                    s_a[i1].rmax + s_b[i2].rmax - 1
+                } else {
+                    0
+                }
             } else if i2 == 0 {
-                rmin = Sa[i1].rmin;
-                rmax = if Sa[i1].rmax + Sb[i2].rmax > 0 { Sa[i1].rmax + Sb[i2].rmax - 1 } else { 0 }
+                rmin = s_a[i1].rmin;
+                rmax = if s_a[i1].rmax + s_b[i2].rmax > 0 {
+                    s_a[i1].rmax + s_b[i2].rmax - 1
+                } else {
+                    0
+                }
             } else {
-                rmin = Sa[i1].rmin + Sb[i2 - 1].rmin;
-                rmax = Sa[i1].rmax + Sb[i2 - 1].rmax;
+                rmin = s_a[i1].rmin + s_b[i2 - 1].rmin;
+                rmax = s_a[i1].rmax + s_b[i2 - 1].rmax;
             }
 
             i1 += 1;
         } else {
-            if 0 < i1 && i1 < Sa.len() {
-                rmin = Sa[i1 - 1].rmin + Sb[i2].rmin;
-                rmax = if Sa[i1].rmax + Sb[i2].rmax > 0 { Sa[i1].rmax + Sb[i2].rmax - 1 } else { 0 };
+            if 0 < i1 && i1 < s_a.len() {
+                rmin = s_a[i1 - 1].rmin + s_b[i2].rmin;
+                rmax = if s_a[i1].rmax + s_b[i2].rmax > 0 {
+                    s_a[i1].rmax + s_b[i2].rmax - 1
+                } else {
+                    0
+                };
             } else if i1 == 0 {
-                rmin = Sb[i2].rmin;
-                rmax = if Sa[i1].rmax + Sb[i2].rmax > 0 { Sa[i1].rmax + Sb[i2].rmax - 1 } else { 0 };
+                rmin = s_b[i2].rmin;
+                rmax = if s_a[i1].rmax + s_b[i2].rmax > 0 {
+                    s_a[i1].rmax + s_b[i2].rmax - 1
+                } else {
+                    0
+                };
             } else {
-                rmin = Sa[i1 - 1].rmin + Sb[i2].rmin;
-                rmax = Sa[i1 - 1].rmax + Sb[i2].rmax;
+                rmin = s_a[i1 - 1].rmin + s_b[i2].rmin;
+                rmax = s_a[i1 - 1].rmax + s_b[i2].rmax;
             }
 
             i2 += 1;
         }
 
         let rank_info = RankInfo::new(val, rmin, rmax);
-        Sm.push(rank_info);
+        s_m.push(rank_info);
     }
 
-    Sm
+    s_m
 }
 
-fn compress<T: Clone + std::fmt::Debug>(S0: &Vec<RankInfo<T>>, B: usize, epsilon: f64) -> Vec<RankInfo<T>> {
-    let mut Sc = Vec::new();
+fn compress<T: Clone + std::fmt::Debug>(s0: &[RankInfo<T>], block_size: usize, epsilon: f64) -> Vec<RankInfo<T>> {
+    let mut s_c = Vec::new();
 
-    let mut S0_range = 0;
+    let mut s0_range = 0;
     let mut e: f64 = 0.0;
 
-    for r in S0 {
-        if S0_range < r.rmax {
-            S0_range = r.rmax;
+    for r in s0 {
+        if s0_range < r.rmax {
+            s0_range = r.rmax;
         }
 
         if (r.rmax - r.rmin) as f64 > e {
@@ -221,30 +237,29 @@ fn compress<T: Clone + std::fmt::Debug>(S0: &Vec<RankInfo<T>>, B: usize, epsilon
         }
     }
 
-    let epsilonN: f64 = epsilon * (S0_range as f64);
-    assert!(2.0 * epsilonN >= e, "precision condition violated.");
+    let epsilon_n: f64 = epsilon * (s0_range as f64);
+    assert!(2.0 * epsilon_n >= e, "precision condition violated.");
 
     let mut i = 0;
     let mut j = 0;
-    while i <= B && j < S0.len() {
-        let r = ((i as f64) * (S0_range as f64) / (B as f64)).floor() as i64;
+    while i <= block_size && j < s0.len() {
+        let r = ((i as f64) * (s0_range as f64) / (block_size as f64)).floor() as i64;
 
-
-        while j < S0.len() {
-            if S0[j].rmax >= r {
+        while j < s0.len() {
+            if s0[j].rmax >= r {
                 break;
             }
 
             j += 1;
         }
 
-        assert!(j < S0.len(), "unable to find the summary with precision.");
-        Sc.push(S0[j].clone());
+        assert!(j < s0.len(), "unable to find the summary with precision.");
+        s_c.push(s0[j].clone());
         j += 1;
         i += 1;
     }
 
-    return Sc;
+    s_c
 }
 
 #[cfg(test)]
@@ -253,45 +268,45 @@ mod tests {
 
     #[test]
     fn test_merge_and_compress() {
-        let mut S0 = Vec::new();
-        let mut S1 = Vec::new();
+        let mut s0 = Vec::new();
+        let mut s1 = Vec::new();
 
-        S0.push(RankInfo::new(2, 1, 1));
-        S0.push(RankInfo::new(4, 3, 4));
-        S0.push(RankInfo::new(8, 5, 6));
-        S0.push(RankInfo::new(17, 8, 8));
+        s0.push(RankInfo::new(2, 1, 1));
+        s0.push(RankInfo::new(4, 3, 4));
+        s0.push(RankInfo::new(8, 5, 6));
+        s0.push(RankInfo::new(17, 8, 8));
 
-        S1.push(RankInfo::new(1, 1, 1));
-        S1.push(RankInfo::new(7, 3, 3));
-        S1.push(RankInfo::new(12, 5, 6));
-        S1.push(RankInfo::new(15, 8, 8));
+        s1.push(RankInfo::new(1, 1, 1));
+        s1.push(RankInfo::new(7, 3, 3));
+        s1.push(RankInfo::new(12, 5, 6));
+        s1.push(RankInfo::new(15, 8, 8));
 
-        let merged = merge(&S0, &S1);
+        let merged = merge(&s0, &s1);
 
         assert_eq!(merged.len(), 8);
-        let merged_vals: Vec<i32> = merged.iter().map(|x| x.val ).collect();
-        let merged_rmins: Vec<usize> = merged.iter().map(|x| x.rmin ).collect();
-        let merged_rmaxs: Vec<usize> = merged.iter().map(|x| x.rmax ).collect();
-        assert_eq!(merged_vals, vec![1,2,4,7,8,12,15,17]);
-        assert_eq!(merged_rmins, vec![1,2,4,6,8,10,13,16]);
-        assert_eq!(merged_rmaxs, vec![1,3,6,8,11,13,15,16]);
+        let merged_vals: Vec<i32> = merged.iter().map(|x| x.val).collect();
+        let merged_rmins: Vec<i64> = merged.iter().map(|x| x.rmin).collect();
+        let merged_rmaxs: Vec<i64> = merged.iter().map(|x| x.rmax).collect();
+        assert_eq!(merged_vals, vec![1, 2, 4, 7, 8, 12, 15, 17]);
+        assert_eq!(merged_rmins, vec![1, 2, 4, 6, 8, 10, 13, 16]);
+        assert_eq!(merged_rmaxs, vec![1, 3, 6, 8, 11, 13, 15, 16]);
 
         let epsilon: f64 = 0.2;
         let compressed = compress(&merged, 4, epsilon);
-        let compressed_vals: Vec<i32> = compressed.iter().map(|x| x.val ).collect();
-        assert_eq!(compressed_vals, vec![1,4,7,12,17]);
+        let compressed_vals: Vec<i32> = compressed.iter().map(|x| x.val).collect();
+        assert_eq!(compressed_vals, vec![1, 4, 7, 12, 17]);
     }
 
     #[test]
     fn test_query() {
-        let N = 100;
+        let n = 100;
         let epsilon: f64 = 0.1;
-        let L = ((N as f64).ln() / 2.0_f64.ln()).floor() as usize + 2;
-        let mut S = EpsilonSummary::new(L, epsilon);
+        let l = ((n as f64).ln() / 2.0_f64.ln()).floor() as usize + 2;
+        let mut s = EpsilonSummary::new(l, epsilon);
 
-        let mut records = Vec::with_capacity(N);
-        let mut quantile_ans: Vec<f64> = Vec::with_capacity(N);
-        for i in 0..N {
+        let mut records = Vec::with_capacity(n);
+        let mut quantile_ans: Vec<f64> = Vec::with_capacity(n);
+        for i in 0..n {
             let x = rand::random::<u32>();
             records.push(x);
 
@@ -305,10 +320,10 @@ mod tests {
             quantile_ans.push((real_rank as f64) / ((i + 1) as f64))
         }
 
-        for i in 0..N {
-            S.update(records[i]);
+        for i in 0..n {
+            s.update(records[i]);
 
-            let quantile_estimated = S.query(records[i]);
+            let quantile_estimated = s.query(records[i]);
             dbg!(records[i]);
             dbg!(quantile_estimated);
             dbg!(quantile_ans[i]);
