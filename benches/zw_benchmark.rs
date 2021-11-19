a9e949e07e68b64193e3e8fffe1a87d607b27c45
Paul Meng <me@paulme.ng>
2021-11-19 09:04:33 -0500

    Adding CKMS for benchmark baseline

--- benches/zw_benchmark.rs
+++ benches/zw_benchmark.rs
@@ -1,9 +1,23 @@
 #[macro_use]
 extern crate criterion;
+extern crate quantiles;
 extern crate zw_fast_quantile;
 
 use criterion::Criterion;
+use quantiles::ckms::CKMS;
 use zw_fast_quantile::FixedSizeEpsilonSummary;
+
+fn bench_ckms_quantile() {
+    let n = 10000;
+    let mut ckms = CKMS::<u16>::new(0.1);
+    for i in 1..=n {
+        ckms.insert(i as u16);
+    }
+    for i in 1..=n {
+        let f_i = i as f64;
+        let _ = ckms.query(f_i);
+    }
+}
 
 fn bench_zw_quantile() {
     let epsilon = 0.1;
@@ -20,6 +34,7 @@ fn bench_zw_quantile() {
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("zw quantile", |b| b.iter(bench_zw_quantile));
+    c.bench_function("ckms quantile", |b| b.iter(bench_ckms_quantile));
 }
 
 criterion_group!(benches, criterion_benchmark);
