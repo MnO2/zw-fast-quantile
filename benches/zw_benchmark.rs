3dbd666e02290e336a6b0e9e2d65570887a4d2b2
Paul Meng <me@paulme.ng>
2021-12-26 23:22:44 -0500

    Benchmark the query and update time separately

--- benches/zw_benchmark.rs
+++ benches/zw_benchmark.rs
@@ -7,7 +7,7 @@ use criterion::Criterion;
 use quantiles::greenwald_khanna;
 use zw_fast_quantile::UnboundEpsilonSummary;
 
-fn bench_gk_quantile() {
+fn bench_gk_quantile_update() -> greenwald_khanna::Stream<usize> {
     let n = 1000;
     let epsilon: f64 = 0.01;
 
@@ -15,9 +15,11 @@ fn bench_gk_quantile() {
     for i in 1..n {
         stream.insert(i);
     }
+
+    stream
 }
 
-fn bench_zw_quantile() {
+fn bench_zw_quantile_update() -> UnboundEpsilonSummary<usize> {
     let n = 1000;
     let epsilon: f64 = 0.01;
 
@@ -25,11 +27,31 @@ fn bench_zw_quantile() {
     for i in 1..=n {
         s.update(i);
     }
+
+    s
+}
+
+fn bench_gk_quantile_query(s: &mut greenwald_khanna::Stream<usize>) {
+    for rank in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0] {
+        s.quantile(rank);
+    }
+}
+
+fn bench_zw_quantile_query(s: &mut UnboundEpsilonSummary<usize>) {
+    for rank in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0] {
+        s.query(rank);
+    }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("zw quantile", |b| b.iter(bench_zw_quantile));
-    c.bench_function("gk quantile", |b| b.iter(bench_gk_quantile));
+    c.bench_function("zw quantile update", |b| b.iter(bench_zw_quantile_update));
+    c.bench_function("gk quantile update", |b| b.iter(bench_gk_quantile_update));
+
+    let mut s = bench_zw_quantile_update();
+    c.bench_function("zw quantile query", |b| b.iter(|| bench_zw_quantile_query(&mut s)));
+
+    let mut stream = bench_gk_quantile_update();
+    c.bench_function("gk quantile query", |b| b.iter(|| bench_gk_quantile_query(&mut stream)));
 }
 
 criterion_group!(benches, criterion_benchmark);
