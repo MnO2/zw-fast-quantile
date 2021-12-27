f6743abff8e1e78a7b2f153fbf676f01480fe4a1
Paul Meng <me@paulme.ng>
2021-12-27 13:22:40 -0500

    Add benchmark for fixed summary

--- benches/zw_benchmark.rs
+++ benches/zw_benchmark.rs
@@ -5,7 +5,7 @@ extern crate zw_fast_quantile;
 
 use criterion::Criterion;
 use quantiles::greenwald_khanna;
-use zw_fast_quantile::UnboundEpsilonSummary;
+use zw_fast_quantile::{UnboundEpsilonSummary, FixedSizeEpsilonSummary};
 
 fn bench_gk_quantile_update() -> greenwald_khanna::Stream<usize> {
     let n = 5000;
@@ -19,11 +19,23 @@ fn bench_gk_quantile_update() -> greenwald_khanna::Stream<usize> {
     stream
 }
 
-fn bench_zw_quantile_update() -> UnboundEpsilonSummary<usize> {
+fn bench_unbound_zw_quantile_update() -> UnboundEpsilonSummary<usize> {
     let n = 5000;
     let epsilon: f64 = 0.01;
 
     let mut s = UnboundEpsilonSummary::new(epsilon);
+    for i in 1..=n {
+        s.update(i);
+    }
+
+    s
+}
+
+fn bench_fixed_zw_quantile_update() -> FixedSizeEpsilonSummary<usize> {
+    let n = 5000;
+    let epsilon: f64 = 0.01;
+
+    let mut s = FixedSizeEpsilonSummary::new(n, epsilon);
     for i in 1..=n {
         s.update(i);
     }
@@ -37,18 +49,19 @@ fn bench_gk_quantile_query(s: &mut greenwald_khanna::Stream<usize>) {
     }
 }
 
-fn bench_zw_quantile_query(s: &mut UnboundEpsilonSummary<usize>) {
+fn bench_unbound_zw_quantile_query(s: &mut UnboundEpsilonSummary<usize>) {
     for rank in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] {
         s.query(rank);
     }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("zw quantile update", |b| b.iter(bench_zw_quantile_update));
+    c.bench_function("zw fixed quantile update", |b| b.iter(bench_fixed_zw_quantile_update));
+    c.bench_function("zw unbound quantile update", |b| b.iter(bench_unbound_zw_quantile_update));
     c.bench_function("gk quantile update", |b| b.iter(bench_gk_quantile_update));
 
-    let mut s = bench_zw_quantile_update();
-    c.bench_function("zw quantile query", |b| b.iter(|| bench_zw_quantile_query(&mut s)));
+    let mut s = bench_unbound_zw_quantile_update();
+    c.bench_function("zw unbound quantile query", |b| b.iter(|| bench_unbound_zw_quantile_query(&mut s)));
 
     let mut stream = bench_gk_quantile_update();
     c.bench_function("gk quantile query", |b| b.iter(|| bench_gk_quantile_query(&mut stream)));
